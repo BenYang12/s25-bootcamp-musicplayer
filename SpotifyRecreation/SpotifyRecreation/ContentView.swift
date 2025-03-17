858f81992ff7a04237393f5804820e1d0c950bc3
Ben Yang <yangbenjamin2005@gmail.com>
2025-03-17 19:06:34 -0400

    no pic

--- SpotifyRecreation/SpotifyRecreation/ContentView.swift
+++ SpotifyRecreation/SpotifyRecreation/ContentView.swift
@@ -2,24 +2,60 @@
 //  ContentView.swift
 //  SpotifyRecreation
 //
-//  Created by Benjamin Yang on 3/17/25.
+//  Created by Benjamin Yang on 3/12/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            Text("testing")
+        VStack{
+            HStack{
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.green)
+                Spacer()
+                
+                Image(systemName: "ellipsis")
+                    .font(.title2)
+            }
+            .padding(.horizontal)
+        
+            Image("currents")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 380, height: 500)
+                .clipShape(RoundedRectangle(cornerRadius: 8))
+            
+            
+            VStack(spacing: 4){
+                Text("Let It Happen")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                
+                Text("Tame Impala")
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                
+            }
+            
+           
         }
-        .padding()
+        
+        Image("slider")
+            .resizable()
+            .scaledToFit()
+            .frame(maxWidth: .infinity)
+        .padding(.horizontal)
+        
+        
+        Spacer()
+        
     }
+        
+
 }
 
 #Preview {
     ContentView()
 }
+
