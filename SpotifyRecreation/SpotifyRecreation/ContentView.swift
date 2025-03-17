0de0de55f2eb7462cf805b3266fc589f67087026
Ben Yang <yangbenjamin2005@gmail.com>
2025-03-17 18:59:48 -0400

    first commit

---
+++ SpotifyRecreation/SpotifyRecreation/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SpotifyRecreation
+//
+//  Created by Benjamin Yang on 3/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("testing")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpotifyRecreation/SpotifyRecreation/SpotifyRecreationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyRecreationApp.swift
+//  SpotifyRecreation
+//
+//  Created by Benjamin Yang on 3/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyRecreationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
